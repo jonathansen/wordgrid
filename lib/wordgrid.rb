5c700e374bb009428926566bdb49deee7807547c
Jonathan Hansen <jonathansen@gmail.com>
2012-04-03 00:23:59 -0400

    Added more validation
    grid can only contain letters
    grid must be a square. Although, this may not matter.

--- lib/wordgrid.rb
+++ lib/wordgrid.rb
@@ -5,5 +5,19 @@ class Wordgrid
     @grid = Matrix[]
   end
   
-  attr_accessor :grid
+  attr_reader :grid
+
+  def grid=(new_grid)
+    # make sure each cell is a single letter character
+    bad_cell = new_grid.find_index{|cell| cell.match(/^[^A-Za-z]$/) }
+    if bad_cell
+      raise ArgumentError "Bad cell at " + bad_cell.to_s
+    end
+
+    unless new_grid.square?
+      raise ArgumentError "grid must be a square."
+    end
+
+    @grid = new_grid
+  end
 end
--- test/wordgrid_spec.rb
+++ test/wordgrid_spec.rb
@@ -12,4 +12,37 @@ describe Wordgrid, "#grid" do
     wordgrid.grid = grid
     wordgrid.grid.should eq(grid)
   end
+
+  it "throws an error if any of the cells are numbers" do
+    wordgrid = Wordgrid.new
+    grid = Matrix[
+      [ "a", "b", "c" ],
+      [ "d", "9", "f" ],
+      [ "g", "h", "i" ]
+    ]
+
+    expect { wordgrid.grid = grid }.to raise_error
+  end
+
+  it "throws an error if any of the cells are not letters" do
+    wordgrid = Wordgrid.new
+    grid = Matrix[
+      [ "a", "b", "c" ],
+      [ "d", "e", "f" ],
+      [ "g", "~", "i" ]
+    ]
+
+    expect { wordgrid.grid = grid }.to raise_error
+  end
+
+  it "throws an error if the grid is not a square" do
+    wordgrid = Wordgrid.new
+    grid = Matrix[
+      [ "a", "b", "c", "j" ],
+      [ "d", "e", "f", "k" ],
+      [ "g", "h", "i", "l" ]
+    ]
+
+    expect { wordgrid.grid = grid }.to raise_error
+  end
 end
